b372540b55334ee6a75c92061e615576e8a8d680
jofrony <jofrony@gmail.com>
2021-03-28 15:24:06 +0200

    old setup tested

--- tests/test_previous_setup.py
+++ tests/test_previous_setup.py
@@ -1,10 +1,12 @@
 from neuromodulation.previous_setup import old_setup
-
+import os
+import pathlib
 
 def test_old_setup():
 
-    dirpath = 'tests/test_data'
+    abs_path =  os.path.dirname(os.path.abspath(__file__))
+    test_dir_path = pathlib.Path(abs_path,'test_data')
 
-    test_setup = old_setup(dirpath)
+    test_setup = old_setup(test_dir_path)
 
     assert test_setup.return_population() == 200
