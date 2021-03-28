396aa711b2f7bb5e775e7ce8f0060eedeed32623
jofrony <jofrony@gmail.com>
2021-03-28 15:11:24 +0200

    experimental data test

---
+++ tests/test_experimental_data.py
@@ -0,0 +1,12 @@
+from neuromodulation.experimental_data import experimental
+
+
+def test_experimental():
+
+    exp_trial = experimental()
+
+    exp_trial.define_exp(mean = 2)
+
+    assert exp_trial.experimental_data['mean'] == 2
+
+
