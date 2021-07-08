09c387bcc6a0c4ff8a54b8017e11599c2615e1b0
jofrony <jofrony@gmail.com>
2021-07-08 19:49:51 +0200

    fix fail build

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,8 +3,9 @@ import os
 import pathlib
 
 
+
 def test_combine_neuromodulators():
-    abs_path = os.path.dirname(os.path.abspath(__file__))
-    test_dir_path = pathlib.Path(abs_path, 'test_combine')
+
+    test_dir_path = pathlib.Path('Neuromodcell/tests/test_combine')
 
     combine_neuromodulators(dir_path=test_dir_path)
