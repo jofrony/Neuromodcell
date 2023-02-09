e42951f6c560a59888c24da7fbfefb2e73d312b8
jofrony <jofrony@gmail.com>
2023-02-09 14:38:43 +0100

    new files

--- tests/test_NrnSimulatorParallel.py
+++ tests/test_NrnSimulatorParallel.py
@@ -6,12 +6,12 @@ def test_NrnSimulatorParallel():
 
     assert hasattr(sim_test, 'run') == True
 
-    sim_test = NrnSimulatorParallel(cvode_active=True)
+    sim_test = NrnSimulatorParallel(cvode_active=True, dt=0.1)
 
     assert sim_test.neuron.h.cvode_active() == 1
 
     sim_test = NrnSimulatorParallel(cvode_active=True)
-    sim_test.run(tstop=10, dt=0.1)
+    sim_test.run(tstop=10)
 
     assert sim_test.neuron.h.tstop == 10
     assert sim_test.neuron.h.dt == 0.1
